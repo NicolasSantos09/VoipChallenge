3668229c0202376c3fa96e2a22c0d1aadab9d6f2
Nicolas <nicolas.a.santos09@gmail.com>
2020-01-07 08:23:30 -0400

    Created new xcode project

---
+++ VoipChallenge/VoipChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VoipChallenge
+//
+//  Created by Nicolas de Andrade dos Santos on 07/01/20.
+//  Copyright © 2020 Nicolas Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
